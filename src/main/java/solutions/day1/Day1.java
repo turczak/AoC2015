7156c1aa3b35c46cc3f255b0055f17a8e9fc6ae2
turczak <wojciech.turczak97@gmail.com>
2023-12-22 10:23:12 +0100

    day 1 & 2 pretty format fix

--- src/main/java/solutions/day1/Day1.java
+++ src/main/java/solutions/day1/Day1.java
@@ -3,11 +3,11 @@ package solutions.day1;
 import java.util.List;
 
 public class Day1 {
-    public int calculateFloor(List<Character> inputList){
+    public int calculateFloor(List<Character> inputList) {
         int floor = 0;
-        for (Character c:
-        inputList){
-            if(c == '('){
+        for (Character c :
+                inputList) {
+            if (c == '(') {
                 floor++;
             } else if (c == ')') {
                 floor--;
@@ -15,11 +15,12 @@ public class Day1 {
         }
         return floor;
     }
-    public int findPositionOfCharacter(List<Character> inputList){
+
+    public int findPositionOfCharacter(List<Character> inputList) {
         int floor = 0;
         int position = 0;
-        while (floor != -1){
-            if(inputList.get(position) == '('){
+        while (floor != -1) {
+            if (inputList.get(position) == '(') {
                 floor++;
             } else if (inputList.get(position) == ')') {
                 floor--;
--- src/main/java/solutions/day2/Day2.java
+++ src/main/java/solutions/day2/Day2.java
@@ -7,20 +7,23 @@ public class Day2 {
 
     private Integer totalSquareFeet = 0, totalLengthOfRibbon = 0;
 
-    public Day2(List<List<Integer>> lists){
+    public Day2(List<List<Integer>> lists) {
         calculateResults(lists);
     }
-    private void calculateResults(List<List<Integer>> lists){
-        for (List<Integer> list:
-        lists){
+
+    private void calculateResults(List<List<Integer>> lists) {
+        for (List<Integer> list :
+                lists) {
             Present present = new Present(list);
             totalSquareFeet += present.getTotalArea();
             totalLengthOfRibbon += present.getTotalLength();
         }
     }
+
     public Integer getTotalSquareFeet() {
         return totalSquareFeet;
     }
+
     public Integer getTotalLengthOfRibbon() {
         return totalLengthOfRibbon;
     }
