c2cc7d297f6eca7c2b1b1ee7140f6ad888a93612
turczak <wojciech.turczak97@gmail.com>
2025-02-27 16:24:45 +0100

    CustomFileReader as single method in Main

--- src/main/java/com/adventofcode/Main.java
+++ src/main/java/com/adventofcode/Main.java
@@ -3,12 +3,29 @@ package com.adventofcode;
 import com.adventofcode.day01.Day01;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        CustomFileReader fileReader = new CustomFileReader(new File("src/main/resources/inputs/day01.txt"));
+        File file;
         /*Day 1*/
+        file = new File("src/main/resources/inputs/day01.txt");
         Day01 day01 = new Day01();
-        System.out.println("Day 1, part I result: " + day01.calculateFloor(fileReader.inputAsListOfCharacters()));
+        System.out.println("Day 1, part I result: " + day01.calculateFloor(inputAsListOfCharacters(file)));
+    }
+
+    private static List<Character> inputAsListOfCharacters(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            return scanner.next()
+                    .chars()
+                    .mapToObj(c -> (char) c)
+                    .toList();
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException("File not found: " + file.getAbsolutePath(), exception);
+        }
     }
 }
