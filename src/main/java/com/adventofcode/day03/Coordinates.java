e04dd5877b8d00b9a05092d3fd50f6c9f63c63db
turczak <wojciech.turczak97@gmail.com>
2025-02-27 22:07:00 +0100

    I'm using now record Coordinates instead of class Point

---
+++ src/main/java/com/adventofcode/day03/Coordinates.java
@@ -0,0 +1,4 @@
+package com.adventofcode.day03;
+
+public record Coordinates(int x, int y) {
+}
--- src/main/java/com/adventofcode/day03/Day03.java
+++ src/main/java/com/adventofcode/day03/Day03.java
@@ -1,28 +1,31 @@
 package com.adventofcode.day03;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.List;
 
 public class Day03 {
 
-    private final HashMap<Point, Integer> visitedHouses = new HashMap<>();
-    private Point coords = new Point(0, 0);
+    private final HashMap<Coordinates, Integer> visitedHouses = new HashMap<>();
 
     public int deliverPresents(List<Character> instructions) {
+        Coordinates coords = new Coordinates(0, 0);
         //Santa begins by delivering a present to the house at his starting location
         visitedHouses.put(coords, visitedHouses.getOrDefault(coords, 0) + 1);
-        instructions.forEach(this::move);
+        for (char direction : instructions) {
+            coords = move(coords, direction);
+            visitedHouses.put(coords, visitedHouses.getOrDefault(coords, 0) + 1);
+        }
         return this.visitedHouses.size();
     }
 
-    private void move(char direction) {
+    private Coordinates move(Coordinates coords, char direction) {
+        Coordinates newCoords = coords;
         switch (direction) {
-            case '>' -> coords = new Point(coords.x + 1, coords.y);
-            case '<' -> coords = new Point(coords.x - 1, coords.y);
-            case '^' -> coords = new Point(coords.x, coords.y + 1);
-            case 'v' -> coords = new Point(coords.x, coords.y - 1);
+            case '>' -> newCoords = new Coordinates(coords.x() + 1, coords.y());
+            case '<' -> newCoords = new Coordinates(coords.x() - 1, coords.y());
+            case '^' -> newCoords = new Coordinates(coords.x(), coords.y() + 1);
+            case 'v' -> newCoords = new Coordinates(coords.x(), coords.y() - 1);
         }
-        visitedHouses.put(coords, visitedHouses.getOrDefault(coords, 0) + 1);
+        return newCoords;
     }
 }
