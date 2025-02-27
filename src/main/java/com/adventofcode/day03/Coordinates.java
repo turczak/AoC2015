f2b7cfca3dd1811729b6dd879856fdf3e4a42ce4
turczak <wojciech.turczak97@gmail.com>
2025-02-27 22:27:37 +0100

    this. is obsolete

--- src/main/java/com/adventofcode/day03/Coordinates.java
+++ src/main/java/com/adventofcode/day03/Coordinates.java
@@ -3,10 +3,10 @@ package com.adventofcode.day03;
 public record Coordinates(int x, int y) {
 
     public Coordinates withX(int x) {
-        return new Coordinates(x, this.y);
+        return new Coordinates(x, y);
     }
 
     public Coordinates withY(int y) {
-        return new Coordinates(this.x, y);
+        return new Coordinates(x, y);
     }
 }
