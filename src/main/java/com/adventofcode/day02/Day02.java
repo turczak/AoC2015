e6033a9fdaf94546af8ac367ddca645299004fd1
turczak <wojciech.turczak97@gmail.com>
2025-02-27 18:26:59 +0100

    removed length, width, height fields to simplify

--- src/main/java/com/adventofcode/day02/Day02.java
+++ src/main/java/com/adventofcode/day02/Day02.java
@@ -4,26 +4,15 @@ import java.util.List;
 
 public class Day02 {
 
-    private int length;
-    private int height;
-    private int width;
-
     public int getTotalSquareFeet(List<List<Integer>> listOfDimensions) {
         int total = 0;
-        for (List<Integer> dimensions : listOfDimensions){
-            updateDimensions(dimensions);
-            int area1 = 2 * length * width;
-            int area2 = 2 * width * height;
-            int area3 = 2 * height * length;
+        for (List<Integer> dimensions : listOfDimensions) {
+            int area1 = 2 * dimensions.get(0) * dimensions.get(2);
+            int area2 = 2 * dimensions.get(2) * dimensions.get(1);
+            int area3 = 2 * dimensions.get(1) * dimensions.get(0);
             int slack = Math.min(Math.min(area1, area2), area3) / 2;
             total += area1 + area2 + area3 + slack;
         }
         return total;
-    }
-
-    private void updateDimensions(List<Integer> dimensions){
-        this.length = dimensions.get(0);
-        this.height = dimensions.get(1);
-        this.width = dimensions.get(2);
     }
 }
