cf1d49894131fec96236760b4d34e7e01a4e1afa
turczak <wojciech.turczak97@gmail.com>
2025-02-27 15:35:57 +0100

    I created CustomFileReader to use in following days. Day 1 part I solution using stream.

--- src/main/java/com/adventofcode/Main.java
+++ src/main/java/com/adventofcode/Main.java
@@ -1,7 +1,14 @@
 package com.adventofcode;
 
+import com.adventofcode.day01.Day01;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CustomFileReader fileReader = new CustomFileReader(new File("src/main/resources/inputs/day01.txt"));
+        /*Day 1*/
+        Day01 day01 = new Day01();
+        System.out.println("Day 1, part I result: " + day01.calculateFloor(fileReader.inputAsListOfCharacters()));
     }
 }
