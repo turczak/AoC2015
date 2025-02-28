28eb4d70255b592ba1958d691f6c1c17c0f9eb5f
turczak <wojciech.turczak97@gmail.com>
2025-02-28 08:07:29 +0100

    beauty also used count() instead of list.size()

--- src/main/java/com/adventofcode/day05/Day05.java
+++ src/main/java/com/adventofcode/day05/Day05.java
@@ -1,6 +1,7 @@
 package com.adventofcode.day05;
 
 import java.util.List;
+import java.util.Map;
 
 public class Day05 {
 
@@ -10,20 +11,20 @@ public class Day05 {
     private static final String PAIR_LETTERS_TWICE_WITHOUT_OVERLAPPING = ".*([a-zA-Z]{2}).*\\1.*";
     private static final String A_B_A_PATTERN = ".*([a-z])\\w(\\1).*";
 
+    private static final Map<CheckingMethod, List<String>> RULES = Map.of(
+            CheckingMethod.STANDARD, List.of(THREE_VOWELS_PATTERN, ONE_LETTER_TWICE_IN_ROW_PATTERN, NO_BANNED_WORDS_PATTERN),
+            CheckingMethod.BETTER, List.of(PAIR_LETTERS_TWICE_WITHOUT_OVERLAPPING, A_B_A_PATTERN)
+    );
+
     public int howManyStringsAreNice(List<String> list, CheckingMethod method) {
-        return list.stream()
+        return (int) list.stream()
                 .filter(word -> isNice(word, method))
-                .toList()
-                .size();
+                .count();
     }
 
     private boolean isNice(String word, CheckingMethod method) {
-        return switch (method) {
-            case STANDARD ->
-                    word.matches(THREE_VOWELS_PATTERN) && word.matches(ONE_LETTER_TWICE_IN_ROW_PATTERN) && word.matches(NO_BANNED_WORDS_PATTERN);
-            case BETTER -> word.matches(PAIR_LETTERS_TWICE_WITHOUT_OVERLAPPING) && word.matches(A_B_A_PATTERN);
-            case null -> false;
-        };
+        return RULES.getOrDefault(method, List.of())
+                .stream()
+                .allMatch(word::matches);
     }
-
 }
