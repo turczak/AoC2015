b3fc23e75f7827b4631d5d38a8931ed59ccf36c3
turczak <wojciech.turczak97@gmail.com>
2025-03-03 20:34:58 +0100

    simpler

--- src/main/java/com/adventofcode/day08/Day08.java
+++ src/main/java/com/adventofcode/day08/Day08.java
@@ -4,18 +4,19 @@ import java.util.List;
 
 public class Day08 {
 
-    public int calculate(List<String> list, boolean encoded) {
-        return list.stream().mapToInt(word -> {
-            if (encoded) {
-                return word.replace("\\", "aa")
+    private static final String HEXADECIMAL = "\\\\x[0-9a-fA-F]{2}";
+
+    public int calculate(List<String> list) {
+        return list.stream().mapToInt(word -> word.length() - word.substring(1, word.length() - 1)
+                .replace("\\\\", "a")
+                .replace("\\\"", "b")
+                .replaceAll(HEXADECIMAL, "c").length()).sum();
+    }
+
+    public int calculateEncoded(List<String> list) {
+        return list.stream()
+                .mapToInt(word -> word.replace("\\", "aa")
                         .replace("\"", "bb")
-                        .length() - word.length() + 2;
-            } else {
-                return word.length() - word.substring(1, word.length() - 1)
-                        .replace("\\\\", "a")
-                        .replace("\\\"", "b")
-                        .replaceAll("\\\\x[0-9a-fA-F]{2}", "c").length();
-            }
-        }).sum();
+                        .length() - word.length() + 2).sum();
     }
 }
