094e51f3711a676d0b37ae0774ea6a8530cc9fa3
turczak <wojciech.turczak97@gmail.com>
2025-03-04 12:56:59 +0100

    containsBannedLetters as method instead of regex

--- src/main/java/com/adventofcode/day11/Day11.java
+++ src/main/java/com/adventofcode/day11/Day11.java
@@ -2,7 +2,7 @@ package com.adventofcode.day11;
 
 public class Day11 {
 
-    private static final String BANNED_LETTERS = "[^iol]*";
+    private static final char[] BANNED_LETTERS = {'i', 'o', 'l'};
     private static final String TWO_DIFFERENT_NON_OVERLAPPING_PAIRS = "(?:([a-z])\\1.*([a-z])\\2|[a-z]*([a-z])\\3[a-z]*){2,}";
 
     public String generateNewPassword(String oldPassword) {
@@ -14,16 +14,27 @@ public class Day11 {
     }
 
     private boolean checkIfValid(String password) {
-        return increasingStraightOfThreeLetters(password) && password.matches(BANNED_LETTERS) && password.matches(TWO_DIFFERENT_NON_OVERLAPPING_PAIRS);
+        return increasingStraightOfThreeLetters(password) && !checkIfContainsBannedLetters(password) && password.matches(TWO_DIFFERENT_NON_OVERLAPPING_PAIRS);
     }
 
-    public boolean increasingStraightOfThreeLetters(String password) {
+    private boolean increasingStraightOfThreeLetters(String password) {
         for (int i = 0; i < password.length() - 2; i++) {
             char first = password.charAt(i);
             char second = (char) (first + 1);
             char third = (char) (first + 2);
             if (password.charAt(i + 1) == second && password.charAt(i + 2) == third) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkIfContainsBannedLetters(String password) {
+        for (char letter : password.toCharArray()) {
+            for (char bannedLetter : BANNED_LETTERS) {
+                if (letter == bannedLetter) {
+                    return true;
+                }
             }
         }
         return false;
