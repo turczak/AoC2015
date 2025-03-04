055852302e83ecc7e618bee0aa4993e8d9e539cb
turczak <wojciech.turczak97@gmail.com>
2025-03-04 11:52:08 +0100

    if bodies as methods

--- src/main/java/com/adventofcode/day07/Day07.java
+++ src/main/java/com/adventofcode/day07/Day07.java
@@ -18,46 +18,72 @@ public class Day07 {
 
     private boolean doCommand(String line) {
         String[] split = line.split(" ");
-        String key = "";
-        char value = ' ';
         if (split.length == 3) {
-            if (split[0].matches("\\d+")) {
-                value = (char) Integer.parseInt(split[0]);
-            } else if (wires.containsKey(split[0])) {
-                value = wires.get(split[0]);
-            }
-            key = split[2];
+            return handleProvidedValue(split);
         } else if (line.contains("NOT") && wires.containsKey(split[1])) {
-            value = (char) ~wires.get(split[1]);
-            key = split[3];
+            return handleNot(line);
         } else if (line.contains("AND") || line.contains("OR")) {
-            if (wires.containsKey(split[0]) && wires.containsKey(split[2])) {
-                value = switch (split[1]) {
-                    case "AND" -> (char) (wires.get(split[0]) & wires.get(split[2]));
-                    case "OR" -> (char) (wires.get(split[0]) | wires.get(split[2]));
-                    default -> throw new IllegalStateException("Unexpected value: " + split[1]);
-                };
-            } else if (split[0].matches("\\d+") && wires.containsKey(split[2])) {
-                value = (char) (Short.parseShort(split[0]) & wires.get(split[2]));
-            }
-            key = split[4];
+            return handleAndOr(split);
         } else if (line.contains("SHIFT")) {
-            if (wires.containsKey(split[0])) {
-                char shiftValue = (char) Integer.parseInt(split[2]);
-                value = switch (split[1]) {
-                    case "LSHIFT" -> (char) (wires.get(split[0]) << shiftValue);
-                    case "RSHIFT" -> (char) (wires.get(split[0]) >> shiftValue);
-                    default -> throw new IllegalStateException("Unexpected value: " + split[1]);
-                };
-                key = split[4];
-            }
+            return handleShift(split);
         } else {
             return false;
         }
+    }
+
+    private boolean updateWireIfValid(String key, char value) {
         if (!key.isBlank() && value != ' ') {
             wires.put(key, value);
             return true;
         }
         return false;
+    }
+
+    private boolean handleProvidedValue(String[] split) {
+        char value = ' ';
+        if (split[0].matches("\\d+")) {
+            value = (char) Integer.parseInt(split[0]);
+        } else if (wires.containsKey(split[0])) {
+            value = wires.get(split[0]);
+        }
+        String key = split[2];
+        return updateWireIfValid(key, value);
+    }
+
+    private boolean handleNot(String line) {
+        String[] split = line.split(" ");
+        char value = (char) ~wires.get(split[1]);
+        String key = split[3];
+        return updateWireIfValid(key, value);
+    }
+
+    private boolean handleAndOr(String[] split) {
+        char value = ' ';
+        if (wires.containsKey(split[0]) && wires.containsKey(split[2])) {
+            value = switch (split[1]) {
+                case "AND" -> (char) (wires.get(split[0]) & wires.get(split[2]));
+                case "OR" -> (char) (wires.get(split[0]) | wires.get(split[2]));
+                default -> throw new IllegalStateException("Unexpected value: " + split[1]);
+            };
+        } else if (split[0].matches("\\d+") && wires.containsKey(split[2])) {
+            value = (char) (Short.parseShort(split[0]) & wires.get(split[2]));
+        }
+        String key = split[4];
+        return updateWireIfValid(key, value);
+    }
+
+    private boolean handleShift(String[] split) {
+        String key = "";
+        char value = ' ';
+        if (wires.containsKey(split[0])) {
+            char shiftValue = (char) Integer.parseInt(split[2]);
+            value = switch (split[1]) {
+                case "LSHIFT" -> (char) (wires.get(split[0]) << shiftValue);
+                case "RSHIFT" -> (char) (wires.get(split[0]) >> shiftValue);
+                default -> throw new IllegalStateException("Unexpected value: " + split[1]);
+            };
+            key = split[4];
+        }
+        return updateWireIfValid(key, value);
     }
 }
