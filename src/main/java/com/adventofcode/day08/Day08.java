1eeebc71330c27e056233a64f1fa5f902a95f9d5
turczak <wojciech.turczak97@gmail.com>
2025-03-04 12:05:51 +0100

    replace chains as separate methods

--- src/main/java/com/adventofcode/day08/Day08.java
+++ src/main/java/com/adventofcode/day08/Day08.java
@@ -7,16 +7,24 @@ public class Day08 {
     private static final String HEXADECIMAL = "\\\\x[0-9a-fA-F]{2}";
 
     public int calculate(List<String> list) {
-        return list.stream().mapToInt(word -> word.length() - word.substring(1, word.length() - 1)
-                .replace("\\\\", "a")
-                .replace("\\\"", "b")
-                .replaceAll(HEXADECIMAL, "c").length()).sum();
+        return list.stream().mapToInt(this::replace).sum();
     }
 
     public int calculateEncoded(List<String> list) {
         return list.stream()
-                .mapToInt(word -> word.replace("\\", "aa")
-                        .replace("\"", "bb")
-                        .length() - word.length() + 2).sum();
+                .mapToInt(this::replaceEncoded).sum();
+    }
+
+    private int replace(String word) {
+        return word.length() - word.substring(1, word.length() - 1)
+                .replace("\\\\", "a")
+                .replace("\\\"", "b")
+                .replaceAll(HEXADECIMAL, "c").length();
+    }
+
+    private int replaceEncoded(String word) {
+        return word.replace("\\", "aa")
+                .replace("\"", "bb")
+                .length() - word.length() + 2;
     }
 }
