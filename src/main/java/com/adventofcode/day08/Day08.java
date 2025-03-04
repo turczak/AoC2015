c4fb9cbde5262f87712914a6ac1c6f3622972563
turczak <wojciech.turczak97@gmail.com>
2025-03-04 13:06:17 +0100

    I will place subsequent calls into separate lines

--- src/main/java/com/adventofcode/day08/Day08.java
+++ src/main/java/com/adventofcode/day08/Day08.java
@@ -7,12 +7,15 @@ public class Day08 {
     private static final String HEXADECIMAL = "\\\\x[0-9a-fA-F]{2}";
 
     public int calculate(List<String> list) {
-        return list.stream().mapToInt(this::replace).sum();
+        return list.stream()
+                .mapToInt(this::replace)
+                .sum();
     }
 
     public int calculateEncoded(List<String> list) {
         return list.stream()
-                .mapToInt(this::replaceEncoded).sum();
+                .mapToInt(this::replaceEncoded)
+                .sum();
     }
 
     private int replace(String word) {
