bd7e4f284ad891451e8d57ddf518b9db5e0c7b4b
turczak <wojciech.turczak97@gmail.com>
2025-03-04 12:10:04 +0100

    ohh, i see

--- src/main/java/com/adventofcode/day10/Day10.java
+++ src/main/java/com/adventofcode/day10/Day10.java
@@ -15,14 +15,15 @@ public class Day10 {
     private String lookAndSay(String input) {
         StringBuilder result = new StringBuilder();
         int counter = 1;
-        for (int i = 0; i < input.length(); i++) {
-            if (i + 1 < input.length() && input.charAt(i) == input.charAt(i + 1)) {
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1)) {
                 counter++;
             } else {
                 result.append(counter).append(input.charAt(i));
                 counter = 1;
             }
         }
+        result.append(counter).append(input.charAt(input.length() - 1));
         return result.toString();
     }
 }
