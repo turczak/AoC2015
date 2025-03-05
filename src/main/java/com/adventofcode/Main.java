aea567339f90b57a1b2523b0c1874dc3d3e531a0
turczak <wojciech.turczak97@gmail.com>
2025-03-05 11:50:42 +0100

    brightness cannot go below 0! Math.max instead of if statement to return correct brightness run() now is void, because I need two different results

--- src/main/java/com/adventofcode/Main.java
+++ src/main/java/com/adventofcode/Main.java
@@ -10,8 +10,8 @@ import com.adventofcode.day06.Day06;
 import com.adventofcode.day07.Day07;
 import com.adventofcode.day08.Day08;
 import com.adventofcode.day09.Day09;
-import com.adventofcode.day10.Day10;
 import com.adventofcode.day09.SearchType;
+import com.adventofcode.day10.Day10;
 import com.adventofcode.day11.Day11;
 
 import java.io.File;
@@ -93,7 +93,9 @@ public class Main {
         File file = new File("src/main/resources/inputs/day06.txt");
         List<String> input = inputAsListOfStrings(file);
         Day06 day06 = new Day06();
-        System.out.println("Day 6, part I result: " + day06.run(input));
+        day06.run(input);
+        System.out.println("Day 6, part I result: " + day06.howManyLightsAreLit());
+        System.out.println("Day 6, part II result: " + day06.totalBrightness());
     }
 
     private static void day07() {
