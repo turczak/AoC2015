abd8a2f8034d8b138b8d01f4b1a3579921f64486
turczak <wojciech.turczak97@gmail.com>
2025-03-05 10:57:14 +0100

    so I want to catch only instruction that provide value to wire 'b'. I don't get it but ok :D

--- src/main/java/com/adventofcode/Main.java
+++ src/main/java/com/adventofcode/Main.java
@@ -101,9 +101,7 @@ public class Main {
         File file = new File("src/main/resources/inputs/day07.txt");
         List<String> input = inputAsListOfStrings(file);
         System.out.println("Day 7, part I result: " + day07.run(input));
-        File file2 = new File("src/main/resources/inputs/day07_2.txt");
-        List<String> input2 = inputAsListOfStrings(file2);
-        System.out.println("Day 7, part II result: " + day07.runPart2(input2));
+        System.out.println("Day 7, part II result: " + day07.runPart2(input));
     }
 
     private static void day08() {
