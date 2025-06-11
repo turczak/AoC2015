8418505c2593e80341b893a0d54c7d899c089404
turczak <wojciech.turczak97@gmail.com>
2025-06-11 11:09:24 +0200

    added method to count surrounding neighbors

--- src/main/java/com/adventofcode/day18/Day18.java
+++ src/main/java/com/adventofcode/day18/Day18.java
@@ -20,4 +20,22 @@ public class Day18 {
                 .toArray(Light[][]::new);
     }
 
+    private int countNeighbors(int x, int y) {
+        int neighbors = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int neighborX = x + i;
+                int neighborY = y + j;
+                if ((neighborX != 0 || neighborY != 0) && // ignore my position
+                        neighborX >= 0 && neighborX < lights.length && // check borders
+                        neighborY >= 0 && neighborY < lights[0].length && // check borders
+                        lights[neighborX][neighborY].state()) {
+                    neighbors++;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+
 }
