fff423c44d11bcd2c98d978e9a939f52954394d5
turczak <wojciech.turczak97@gmail.com>
2025-06-11 10:42:07 +0200

    actually i want to use class from day 6 to make grid of lights, changed method of parsing to return Light[][]

--- src/main/java/com/adventofcode/day18/Day18.java
+++ src/main/java/com/adventofcode/day18/Day18.java
@@ -1,19 +1,23 @@
 package com.adventofcode.day18;
 
+import com.adventofcode.day06.Light;
+
 import java.util.List;
 
 public class Day18 {
 
-    private char[][] grid = new char[100][100];
+    private Light[][] lights = new Light[100][100];
 
     public Day18(List<String> input) {
-        grid = parseToGrid(input);
+        lights = parseToGrid(input);
     }
 
-    private char[][] parseToGrid(List<String> input) {
+    private Light[][] parseToGrid(List<String> input) {
         return input.stream()
-                .map(String::toCharArray)
-                .toArray(char[][]::new);
+                .map(line -> line.chars()
+                        .mapToObj(character -> new Light(character == '#', 0))
+                        .toArray(Light[]::new))
+                .toArray(Light[][]::new);
     }
 
 }
