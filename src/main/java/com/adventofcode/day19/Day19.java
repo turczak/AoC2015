e96948dfc1e0d9bfed124604af15b3b07b5bfae0
turczak <wojciech.turczak97@gmail.com>
2025-06-12 12:08:32 +0200

    as Map

--- src/main/java/com/adventofcode/day19/Day19.java
+++ src/main/java/com/adventofcode/day19/Day19.java
@@ -1,14 +1,16 @@
 package com.adventofcode.day19;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day19 {
     private static final Pattern REPLACEMENT_PATTERN = Pattern.compile("(\\w+) => (\\w+)");
     private String puzzle;
-    private final List<Replacement> replacements = new ArrayList<>();
+    private final Map<String, List<String>> replacements = new HashMap<>();
 
     public Day19(List<String> input) {
         input.forEach(this::parse);
@@ -19,7 +21,12 @@ public class Day19 {
     private void parse(String line) {
         Matcher matcher = REPLACEMENT_PATTERN.matcher(line);
         if (matcher.find()) {
-            replacements.add(new Replacement(matcher.group(1), matcher.group(2)));
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+            if (!replacements.containsKey(key)) {
+                replacements.put(key, new ArrayList<>());
+            }
+            replacements.get(key).add(value);
         } else {
             puzzle = line;
         }
--- src/main/java/com/adventofcode/day19/Replacement.java
+++
@@ -1,4 +0,0 @@
-package com.adventofcode.day19;
-
-public record Replacement(String from, String to) {
-}
