24e4fb60aac32ac8dfc32a576488089c07122e64
turczak <wojciech.turczak97@gmail.com>
2025-07-14 19:49:37 +0200

    Initial commit of Day 22

---
+++ src/main/java/com/adventofcode/day22/Day22.java
@@ -0,0 +1,11 @@
+package com.adventofcode.day22;
+
+public class Day22 {
+
+    //puzzle
+    private static final int BOSS_HIT_POINTS = 58;
+    private static final int BOSS_DAMAGE = 9;
+    private static final int PLAYER_HIT_POINTS = 50;
+    private static final int MANA_POINTS = 500;
+
+}
